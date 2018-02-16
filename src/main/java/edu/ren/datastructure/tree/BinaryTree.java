5659388334b04dc0d05a46b3e9c396afca83e92e
renuka <renukarajput707@gmail.com>
2018-02-16 16:46:02 +0530

    Binary tree creation and its basic operation

--- src/main/java/edu/ren/datastructure/tree/BinaryTree.java
+++ src/main/java/edu/ren/datastructure/tree/BinaryTree.java
@@ -1,10 +1,10 @@
 package edu.ren.datastructure.tree;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
-public class BinaryTree<T> extends BinarySearchTree {
+public class BinaryTree<T> {
+    Node<T> root;
     public BinaryTree() {
         root = null;
     }
@@ -17,6 +17,7 @@ public class BinaryTree<T> extends BinarySearchTree {
         insert(root, val);
     }
 
+    // Recursive insert
     private boolean insert(Node<T> root, T val) {
         if (root.leftChild == null) {
             root.leftChild = new Node<>(val);
@@ -32,6 +33,183 @@ public class BinaryTree<T> extends BinarySearchTree {
         return isInserted;
     }
 
+    public boolean isTreeSame(BinaryTree<T> t1, BinaryTree<T> t2){
+        if (t1 != null && t2 != null)
+            return isTreeSame(t1.root, t2.root);
+        return false;
+    }
+
+    private boolean isTreeSame(Node<T> node1, Node<T> node2) {
+        if (node1 == null && node2 == null)
+            return true;
+
+        if ((node1.leftChild == null && node2.leftChild == null) && (node1.rightChild == null && node2.rightChild == null))
+            return true;
+
+        if (node1 != null && node2 != null)
+            if ((node1.leftChild == null && node2.leftChild == null ||
+                node1.leftChild != null && node2.leftChild != null )&&(
+                node1.rightChild == null && node2.rightChild == null ||
+                node1.rightChild != null && node2.rightChild != null))
+                    return isTreeSame(node1.leftChild, node2.leftChild) && isTreeSame(node1.rightChild, node2.rightChild);
+        return false;
+    }
+
+        public void deleteGivenNode(Node<T> nodeToDelete) {
+        Node<T> rightMostChild = getRightMostNode(this.root);
+
+        swapNodeValues(nodeToDelete,rightMostChild);
+        Node<T> parentNode = getParentNode(this.root, rightMostChild);
+        System.out.println(parentNode.val);
+        if (parentNode.leftChild != null && parentNode.leftChild.val == rightMostChild.val)
+            parentNode.leftChild = null;
+        if (parentNode.rightChild != null && parentNode.rightChild.val == rightMostChild.val)
+            parentNode.rightChild = null;
+    }
+
+    private void swapNodeValues(Node<T> nodeOne, Node<T> nodeTwo){
+        T temp = nodeOne.val;
+        nodeOne.val=nodeTwo.val;
+        nodeTwo.val=temp;
+
+    }
+
+    public Node<T> getRightMostNode(Node<T> root){
+        if (root.rightChild != null){
+            root = getRightMostNode(root.rightChild);
+        }else{
+            if (root.leftChild != null)
+                root = getRightMostNode(root.leftChild);
+        }
+        return root;
+    }
+
+    public Node<T> getParentNode(Node<T> root, Node<T> nodeToSearch) {
+        Node<T> parent;
+        if (root == null) {
+            return root;
+        } else {
+            if (root.leftChild == nodeToSearch || root.rightChild == nodeToSearch) {
+                parent = root;
+                return parent;
+            } else {
+              parent =  getParentNode(root.leftChild, nodeToSearch);
+                if (parent == null) {
+                    parent = getParentNode(root.rightChild, nodeToSearch);
+                }
+            }
+            return parent;
+        }
+    }
+
+    private void inOrder(Node root) {
+        if (root != null) {
+            inOrder(root.leftChild);
+            System.out.println(root.val);
+            inOrder(root.rightChild);
+        }
+    }
+
+    public void insertInLevelOrder(T value) {
+        insertInLevelOrder(root, new Node<>(value));
+    }
+
+    private void insertInLevelOrder(Node<T> root, Node<T> node) {
+        if (root == null) {
+            this.root = node;
+            return;
+        }
+        List<Node<T>> bfs = new ArrayList<>();
+        bfs.add(root);
+        int position = 0;
+        while (true) {
+            if (root.leftChild != null) {
+                bfs.add(root.leftChild);
+            } else {
+                root.leftChild = node;
+                return;
+            }
+            if (root.rightChild != null) {
+                bfs.add(root.rightChild);
+            } else {
+                root.rightChild = node;
+                return;
+            }
+            position++;
+            root = bfs.get(position);
+        }
+    }
+
+   public void printByBfs(){
+       Queue<Node<T>> queue = new LinkedList<>();
+       Node<T> root = this.root;
+       queue.add(root);
+        while (!queue.isEmpty()){
+            Node<T> qHead = queue.remove();
+            if (qHead.leftChild != null) {
+                queue.add(qHead.leftChild);
+            }
+            if (qHead.rightChild != null){
+                queue.add(qHead.rightChild);
+            }
+            System.out.print(qHead.val + " ");
+        }
+    }
+
+    // using two queue
+    public void printBfsByLine(){
+        Queue<Node<T>> rootQueue = new LinkedList<>();
+        Queue<Node<T>> childQueue = new LinkedList<>();
+        Node<T> root = this.root;
+        rootQueue.add(root);
+        while (!(rootQueue.isEmpty() && childQueue.isEmpty())){
+            Node<T> currentElement = rootQueue.remove();
+            if (currentElement.leftChild != null)
+                childQueue.add(currentElement.leftChild);
+
+            if (currentElement.rightChild != null)
+                childQueue.add(currentElement.rightChild);
+
+            System.out.print(currentElement.val+ " ");
+
+            if(rootQueue.isEmpty()){
+                Queue<Node<T>> tempQueue;
+                tempQueue = rootQueue;
+                rootQueue = childQueue;
+                childQueue = tempQueue;
+                System.out.println();
+            }
+        }
+    }
+
+    // using marker
+public void bfsByLineUsingMarker() {
+    Queue<Node<T>> bfsQueue = new LinkedList<>();
+    Node<T> marker = null;
+    Node<T> root = this.root;
+    bfsQueue.add(root);
+    bfsQueue.add(marker);
+    while (true) {
+        Node<T> currentElement = bfsQueue.remove();
+        if (currentElement == marker) {
+            if (bfsQueue.isEmpty()){
+                break;
+            }
+            System.out.println();
+            bfsQueue.add(marker);
+            continue;
+        }
+        if (currentElement.leftChild != null)
+            bfsQueue.add(currentElement.leftChild);
+        if (currentElement.rightChild != null)
+            bfsQueue.add(currentElement.rightChild);
+        System.out.print(currentElement.val + " ");
+
+    }
+}
+
+
+
     public void insertInBinaryTreee(T val) {
         root = insertInBinaryTree(root, val);
     }
@@ -39,27 +217,21 @@ public class BinaryTree<T> extends BinarySearchTree {
     private Node insertInBinaryTree(Node<T> root, T val) {
         if (root == null) {
             root = new Node(val);
-            return root;
-        } else {
-            if (root.leftChild == null) {
-                root.leftChild = new Node(val);
-                return root;
-            } else {
-                 insertInBinaryTree(root.leftChild, val);
-            }
-            if (root.rightChild == null) {
-                root.rightChild = new Node(val);
-                return root;
-            } else {
-                 insertInBinaryTree(root.rightChild, val);
-            }
+        } else if (root.leftChild == null) {
+            root.leftChild = new Node(val);
+        } else if (root.rightChild == null) {
+            root.rightChild = new Node(val);
+        } else if (root.rightChild != null) { //don't go to left subtree if already inserted in right
+            insertInBinaryTree(root.leftChild, val);
+        } else if (root.leftChild != null) {
+            insertInBinaryTree(root.rightChild, val);
         }
         return root;
     }
 
-    public List<T> preOrderValues(){
+    public List<T> preOrderValues() {
         List<Node<T>> nodes = preOrder();
-        return nodes.stream().map(node->node.val).collect(Collectors.toList());
+        return nodes.stream().map(node -> node.val).collect(Collectors.toList());
     }
 
     public List<Node<T>> preOrder() {
--- src/test/java/edu/ren/datastructure/tree/BinaryTreeTest.java
+++ src/test/java/edu/ren/datastructure/tree/BinaryTreeTest.java
@@ -4,21 +4,29 @@ import org.junit.Test;
 
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
-/**
- * Created by rrn3194 on 1/30/18.
- */
 public class BinaryTreeTest {
 
     @Test
-    public void testInsert(){
-        BinaryTree<Integer> binaryTree=new BinaryTree<>();
+    public void testInsertInLevelOrder() {
+        BinaryTree<Integer> binaryTree = getIntegerBinaryTree(20, 10, 30, 40, 50, 60, 70);
+        System.out.println("\n");
+        BinaryTreePrinter.printNode(binaryTree.root);
+    }
+
+    @Test
+    public void testInsert() {
+        BinaryTree<Integer> binaryTree = new BinaryTree<>();
         binaryTree.insert(20);
         binaryTree.insert(10);
         binaryTree.insert(30);
         binaryTree.insert(40);
         binaryTree.insert(50);
+        binaryTree.insert(60);
+        binaryTree.insert(70);
+
         List<Integer> preOrder = binaryTree.preOrderValues();
         System.out.print(preOrder);
         System.out.println("\n");
@@ -26,13 +34,92 @@ public class BinaryTreeTest {
     }
 
     @Test
-    public void testInsertBroken(){
-        BinaryTree<Integer> binaryTree=new BinaryTree<>();
+    public void testParentNode(){
+
+        BinaryTree<Integer> treeOne = getIntegerBinaryTree(10, 5, 8, 1, 2, 3, 4);
+
+        List<Integer> preOrder = treeOne.preOrderValues();
+        System.out.print(preOrder);
+        System.out.println("\n");
+        BinaryTreePrinter.printNode(treeOne.root);
+
+        Node childNode = treeOne.root.leftChild.rightChild;
+        Node parentNode = treeOne.getParentNode(treeOne.root,childNode);
+        assertThat(parentNode.val,is(5));
+
+        Node childNode1 = treeOne.root.leftChild;
+        Node parentNode1 = treeOne.getParentNode(treeOne.root,childNode1);
+        assertThat(parentNode1.val,is(10));
+
+        Node rightMostNode = (treeOne.getRightMostNode(treeOne.root));
+        assertThat(rightMostNode.val, is(4));
+
+        treeOne.deleteGivenNode(treeOne.root.leftChild);
+        System.out.println("\n");
+        BinaryTreePrinter.printNode(treeOne.root);
+    }
+
+    @Test
+    public void testTreeSimilarity(){
+
+        BinaryTree<Integer> treeOne = getIntegerBinaryTree(10, 5, 8, 1, 2, 3, 4);
+
+        BinaryTree<Integer> treeTwo = getIntegerBinaryTree(10, 5, 8, 1, 2, 3, 4);
+
+        assertThat(treeOne.isTreeSame(treeOne, treeTwo), is(true));
+    }
+
+    @Test
+    public void testTreeDisSimilarity(){
+
+        BinaryTree<Integer> treeOne = getIntegerBinaryTree(10, 5, 8, 1, 2, 3, 4);
+
+        BinaryTree<Integer> treeTwo = getIntegerBinaryTree(10, 5, 8, 1, 2, 3, 4, 11);
+
+        assertThat(treeOne.isTreeSame(treeOne, treeTwo), is(false));
+    }
+
+    @Test
+    public void testBFSLineByLine() {
+
+        BinaryTree<Integer> treeOne = getIntegerBinaryTree(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        BinaryTreePrinter.printNode(treeOne.root);
+        treeOne.printBfsByLine();
+        treeOne.printByBfs();
+        treeOne.bfsByLineUsingMarker();
+    }
+
+    @Test
+    public void testBFSLineByLineUsingMarker() {
+        BinaryTree<Integer> treeOne = getIntegerBinaryTree(1, 2, 3, 4, 5, 6, 7);
+        BinaryTreePrinter.printNode(treeOne.root);
+
+        treeOne.bfsByLineUsingMarker();
+    }
+
+    private BinaryTree<Integer> getIntegerBinaryTree(int... values) {
+        BinaryTree<Integer> treeOne = new BinaryTree<>();
+        for (int value : values) {
+            treeOne.insertInLevelOrder(value);
+        }
+        return treeOne;
+    }
+
+    @Test
+    public void testInsertBroken() {
+        BinaryTree<Integer> binaryTree = new BinaryTree<>();
         binaryTree.insertInBinaryTreee(20);
         binaryTree.insertInBinaryTreee(10);
-        binaryTree.insert(30);
+        binaryTree.insertInBinaryTreee(30);
+        BinaryTreePrinter.printNode(binaryTree.root);
         binaryTree.insertInBinaryTreee(40);
+        BinaryTreePrinter.printNode(binaryTree.root);
         binaryTree.insertInBinaryTreee(50);
+        BinaryTreePrinter.printNode(binaryTree.root);
+        binaryTree.insertInBinaryTreee(60);
+        BinaryTreePrinter.printNode(binaryTree.root);
+        binaryTree.insertInBinaryTreee(70);
+        BinaryTreePrinter.printNode(binaryTree.root);
         List<Integer> preOrder = binaryTree.preOrderValues();
         System.out.print(preOrder);
         System.out.println("\n");
