d5a735b860f2f50332ae04414c2db7e80fccec7d
renuka <renukarajput707@gmail.com>
2018-08-22 00:28:52 +0530

    Cycle detection in list test

--- src/test/java/edu/ren/datastructure/interviewBit/CycleDetectionTest.java
+++ src/test/java/edu/ren/datastructure/interviewBit/CycleDetectionTest.java
@@ -1,60 +1,49 @@
 package edu.ren.datastructure.interviewBit;
 
 import org.junit.Test;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.*;
 
 public class CycleDetectionTest {
-    @Test
-    public void inputWithCycle() {
-        ListNode listNode = new ListNode(1);
-        listNode.next = new ListNode(2);
-        listNode.next.next = new ListNode(3);
-        listNode.next.next.next = new ListNode(4);
-        listNode.next.next.next.next = listNode.next.next;
-
-        ListNode output = new CycleDetection().findStartOfLoop(listNode);
-
-        assertThat(new Integer(output.val).toString(), is("3"));
-    }
 
     @Test
     public void inputWithCycle1() {
-        ListNode listNode = ListNodeProxy.fromArray(new int[]{1,2,3,4,5});
-        ListNodeProxy.createCycle(listNode,3);
+        ListNode listNode = ListNodeProxy.fromArray(new int[]{1, 2, 3, 4, 5});
+        ListNodeProxy.createCycle(listNode, 3);
         ListNode output = new CycleDetection().findStartOfLoop(listNode);
-        assertThat(new Integer(output.val).toString(), is("3"));
+        assertThat(output.val, is(3));
     }
 
     @Test
     public void inputWithCycle2() {
-        ListNode listNode = ListNodeProxy.fromArray(new int[]{1,2,3,4,5});
-        ListNodeProxy.createCycle(listNode,4);
+        ListNode listNode = ListNodeProxy.fromArray(new int[]{1, 2, 3, 4, 5});
+        ListNodeProxy.createCycle(listNode, 4);
         ListNode output = new CycleDetection().findStartOfLoop(listNode);
-        assertThat(new Integer(output.val).toString(), is("4"));
+        assertThat(output.val, is(4));
     }
 
 
     @Test
     public void inputWithCycle3() {
-        ListNode listNode = ListNodeProxy.fromArray(new int[]{1,2,3,4,5,6,7,8,9});
-        ListNodeProxy.createCycle(listNode,6);
+        ListNode listNode = ListNodeProxy.fromArray(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9});
+        ListNodeProxy.createCycle(listNode, 6);
         ListNode output = new CycleDetection().findStartOfLoop(listNode);
-        assertThat(new Integer(output.val).toString(), is("6"));
+        assertThat(output.val, is(6));
     }
 
     @Test
     public void inputWithCycle4() {
-        ListNode listNode = ListNodeProxy.fromArray(new int[]{1,2,3,4,5,6,7,8,9});
-        ListNodeProxy.createCycle(listNode,3);
+        ListNode listNode = ListNodeProxy.fromArray(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9});
+        ListNodeProxy.createCycle(listNode, 3);
         ListNode output = new CycleDetection().findStartOfLoop(listNode);
-        assertThat(new Integer(output.val).toString(), is("3"));
+        assertThat(output.val, is(3));
     }
+
     @Test
     public void inputWithSingleNode() {
         ListNode listNode = new ListNode(1);
-
         ListNode output = new CycleDetection().findStartOfLoop(listNode);
         assertThat(output, is(nullValue()));
     }
@@ -67,23 +56,36 @@ public class CycleDetectionTest {
 
     @Test
     public void inputWithoutCycle() {
-        ListNode listNode = new ListNode(1);
-        listNode.next = new ListNode(2);
-        listNode.next.next = new ListNode(3);
-        listNode.next.next.next = new ListNode(4);
-        listNode.next.next.next.next = new ListNode(5);
-
+        ListNode listNode = ListNodeProxy.fromArray(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9});
         ListNode output = new CycleDetection().findStartOfLoop(listNode);
 
         assertThat(output, is(nullValue()));
     }
 
     @Test
-    public void inputWithCycleLength4() {
-        ListNode listNode = ListNodeProxy.fromArray(new int[]{1,2,3,4,5});
-        ListNodeProxy.createCycle(listNode,3);
+    public void inputWithoutCycle5() {
+        ListNode listNode = ListNodeProxy.fromArray(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9});
+        ListNodeProxy.createCycle(listNode, 2);
+        ListNode output = new CycleDetection().findStartOfLoop(listNode);
+
+        assertThat(output.val, is(2));
+    }
+
+    @Test
+    public void inputWithCycle6() {
+        ListNode listNode = ListNodeProxy.fromArray(new int[]{1, 2, 3, 4});
+        ListNodeProxy.createCycle(listNode, 3);
+        ListNode output = new CycleDetection().findStartOfLoop(listNode);
+
+        assertThat(output.val, is(3));
+    }
+
+    @Test
+    public void inputWithCycle7() {
+        ListNode listNode = ListNodeProxy.fromArray(new int[]{1, 2, 3, 4, 5});
+        ListNodeProxy.createCycle(listNode, 3);
 
         ListNode output = new CycleDetection().findStartOfLoop(listNode);
-        assertThat(new Integer(output.val).toString(), is("3"));
+        assertThat(output.val, is(3));
     }
 }
