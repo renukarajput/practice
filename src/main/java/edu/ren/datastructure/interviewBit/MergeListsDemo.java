705e9bcf6c9934e8d718651390d56baaa653ba8c
renuka <renukarajput707@gmail.com>
2018-08-27 10:28:20 +0530

    Added toString()

--- src/main/java/edu/ren/datastructure/interviewBit/MergeListsDemo.java
+++ src/main/java/edu/ren/datastructure/interviewBit/MergeListsDemo.java
@@ -6,7 +6,12 @@ class ListNode {
     public int val;
     public ListNode next;
     ListNode(int x) { val = x; next = null; }
-  }
+
+   @Override
+    public String toString() {
+        return ListNodeProxy.stringFromListNode(this);
+    }
+}
 
 public class MergeListsDemo {
 
