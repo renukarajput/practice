a3a34cee5c3b2e29af2e92edbb6860d323a15321
renuka <renukarajput707@gmail.com>
2018-09-12 00:27:37 +0530

    Compress string and test modification

--- src/main/java/edu/ren/geeks/programs/StringCompressor.java
+++ src/main/java/edu/ren/geeks/programs/StringCompressor.java
@@ -7,15 +7,33 @@ import java.util.List;
 public class StringCompressor {
     public static void getCompressed(char[] input) {  //aabbcc
         int length = input.length - 1;
+        int writeAt = 1;
         int count = 1;
         for (int i = 0; i <= length; i++) {
-            if (i == length || !(input[i] == input[i+1])){
-                System.out.print(input[i]+""+count);
+            if (i == length || !(input[i] == input[i + 1])) {
+                input[writeAt] = (char) (count + 48);
+                writeAt = writeAt + 2;
                 count = 1;
-            }else {
+            } else {
                 count++;
             }
         }
+    }
+
+
+    public static String compressString(String input) {
+        int length = input.length() - 1;
+        StringBuilder sb = new StringBuilder();
+        int count = 1;
+        for (int i = 0; i <= length; i++) {
+            if (i == length || (input.charAt(i) != input.charAt(i + 1))) {
+                sb.append(input.charAt(i)).append(count);
+                count = 1;
+            } else {
+                count++;
+            }
+        }
+        return sb.toString();
     }
 
     public static int getCompressedArray(char[] input) {
@@ -23,30 +41,30 @@ public class StringCompressor {
         int count = 1;
         int writePosition = 0;
         for (int readPosition = 0; readPosition <= length; readPosition++) {
-            if (readPosition == length || !(input[readPosition] == input[readPosition+1])){
+            if (readPosition == length || !(input[readPosition] == input[readPosition + 1])) {
                 input[writePosition++] = input[readPosition];
 //               List<Integer> digits = getDigitArrayForInt(count);
                 char[] digits = String.valueOf(count).toCharArray();
-                if (count > 9){
+                if (count > 9) {
                     for (int j = 0; j < digits.length; j++) {
-                        input[writePosition++] = Character.forDigit(Character.getNumericValue(digits[j]),10);
+                        input[writePosition++] = Character.forDigit(Character.getNumericValue(digits[j]), 10);
                     }
-                }else{
-                    input[writePosition++] = Character.forDigit(count,10);
+                } else {
+                    input[writePosition++] = Character.forDigit(count, 10);
                 }
                 count = 1;
-            }else{
+            } else {
                 count++;
             }
         }
         return writePosition;
     }
 
-     static List<Integer> getDigitArrayForInt(int number) { //6532
+    static List<Integer> getDigitArrayForInt(int number) { //6532
         List<Integer> list = new ArrayList<>();
-       // Integer[] integers = list.toArray(new Integer[list.size()]);
-        while(number > 0){
-            list.add(0,number % 10);
+        // Integer[] integers = list.toArray(new Integer[list.size()]);
+        while (number > 0) {
+            list.add(0, number % 10);
             number = number / 10;
         }
         return list;
