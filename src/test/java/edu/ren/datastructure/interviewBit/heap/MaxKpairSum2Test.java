c3ee2cf3b52314c9e78c106225be797829994b56
rakesh <rakesh99kr@gmail.com>
2018-10-17 23:22:07 +0530

    max k pairs sum

---
+++ src/test/java/edu/ren/datastructure/interviewBit/heap/MaxKpairSum2Test.java
@@ -0,0 +1,17 @@
+package edu.ren.datastructure.interviewBit.heap;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MaxKpairSum2Test extends MaxKpairSumTest{
+
+    @Override
+    protected MaxKpairSum getMaxKpairSum() {
+        return new MaxKpairSum2();
+    }
+
+    @Test
+    public void maxSum() {
+    }
+}
