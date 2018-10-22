75bb6827eb7a6f63f4ed390f036c005dfd974d58
renuka <renukarajput707@gmail.com>
2018-10-22 12:19:27 +0530

    Check if tree is bst

---
+++ src/main/java/edu/ren/datastructure/interviewBit/tree/BinarySearchCheck.java
@@ -0,0 +1,46 @@
+package edu.ren.datastructure.interviewBit.tree;
+
+public class BinarySearchCheck {
+
+    public boolean isBinarySearchTree(TreeNode root) {
+        if (root == null)
+            return true;
+
+        if (root.left == null && root.right == null)
+            return true;
+
+        if ((root.left != null && ((Comparable) root.left.val).compareTo(root.val) > 0)) {
+            return false;
+        }
+
+        if ((root.right != null && ((Comparable) root.right.val).compareTo(root.val) < 0)) {
+            return false;
+        }
+
+        if (maxOfBst(root.left) > root.val) {
+            return false;
+        }
+
+        if (minOfBst(root.right) < root.val) {
+            return false;
+        }
+        boolean left = isBinarySearchTree(root.left);
+        boolean right = isBinarySearchTree(root.right);
+        return left && right;
+    }
+
+    public int maxOfBst(TreeNode root) {
+        while (root.right != null) {
+            root = root.right;
+        }
+        return root.val;
+    }
+
+    public int minOfBst(TreeNode root) {
+        while (root.left != null) {
+            root = root.left;
+        }
+        return root.val;
+    }
+
+}
