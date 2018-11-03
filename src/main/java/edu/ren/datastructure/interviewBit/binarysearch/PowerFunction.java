e28a5d6d79d4bd8e66ba0f8a5929d48b1ccc2046
rakesh <rakesh99kr@gmail.com>
2018-11-03 12:40:41 +0530

    power function modulo result

--- src/main/java/edu/ren/datastructure/interviewBit/binarysearch/PowerFunction.java
+++ src/main/java/edu/ren/datastructure/interviewBit/binarysearch/PowerFunction.java
@@ -1,7 +1,41 @@
 package edu.ren.datastructure.interviewBit.binarysearch;
 
 public class PowerFunction {
-    int power(int base, int power, int remainder) {
-       return 0;
+    int powerMod(int base, int power, int divisor) {
+        int result = (int)power(base, Math.abs(power), divisor);
+        if (power < 0 && result!=0) {
+            return 1 / result;
+        }
+        if(result<0){
+            result+=divisor;
+        }
+        return result % divisor;
+    }
+
+    /**9^2%5 =((9%5)*(9%5))%5
+     * 81%5=
+     * ((5*1+4)*(5*1+4))%5
+     * =(5*5+5*4+4*5+4*4)%5
+     * =(5*(5+4+4) +4*4)%5
+     * =4*4%5
+     * =1
+     * so num^2%div =((num%d)*(num%d))%d
+     * @param base
+     * @param power
+     * @param divisor
+     * @return
+     */
+    private long power(int base, int power, int divisor) {
+        if (power == 0) {
+            return 1;
+        }
+
+        if (power % 2 != 0) {
+            return (base * power(base, power - 1, divisor))%divisor;
+        }
+        long resultOfHalf = power(base, power / 2, divisor);
+        long square = (resultOfHalf%divisor) *(resultOfHalf%divisor);
+        return square%divisor;
+
     }
 }
--- src/test/java/edu/ren/datastructure/interviewBit/binarysearch/PowerFunctionTest.java
+++ src/test/java/edu/ren/datastructure/interviewBit/binarysearch/PowerFunctionTest.java
@@ -2,15 +2,74 @@ package edu.ren.datastructure.interviewBit.binarysearch;
 
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class PowerFunctionTest {
+    PowerFunction powerFunction = new PowerFunction();
 
     @Test
     public void power() {
-        int num=-7;
-        int divisor=3;
+        int result = powerFunction.powerMod(2, 3, 2);
+        assertThat(result, is(0));
+    }
 
-        System.out.println(num%3);
+    @Test
+    public void power_1() {
+        int result = powerFunction.powerMod(2, 3, 3);
+        assertThat(result, is(2));
+    }
+
+    @Test
+    public void power_OddOfNegativeNum() {
+        int result = powerFunction.powerMod(-2, 3, 2);
+        assertThat(result, is(0));
+
+    }
+
+    @Test
+    public void power_evenOfNegativeNum() {
+        int result = powerFunction.powerMod(-2, 4, 2);
+        assertThat(result, is(0));
+
+    }
+
+    @Test
+    public void power_negative_pow() {
+        int result = powerFunction.powerMod(2, -3, 2);
+        assertThat(result, is(0));
+    }
+
+    @Test
+    public void power_remainder() {
+        int result = powerFunction.powerMod(2, 5, 3);
+        assertThat(result, is(2));
+
+    }
+
+    @Test
+    public void power_remainder_1() {
+        int result = powerFunction.powerMod(3, 4, 2);
+        assertThat(result, is(1));
+
+    }
+
+    @Test
+    public void power_remainder_2() {
+        int result = powerFunction.powerMod(9, 2, 5);
+        assertThat(result, is(1));
+
+    }
+    @Test
+    public void power_remainder_3() {
+        int result = powerFunction.powerMod(-1, 1, 20);
+        assertThat(result, is(19));
+
+    }
+    @Test
+    public void power_remainder_large() {
+        int result = powerFunction.powerMod(71045970, 41535484, 64735492);
+        assertThat(result, is(20805472));
+
     }
 }
