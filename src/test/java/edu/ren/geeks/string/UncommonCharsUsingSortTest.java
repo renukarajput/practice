9e47e2f139b047e1054bcc8035df6415f9e1063e
renuka <renukarajput707@gmail.com>
2018-11-11 19:33:11 +0530

    Reformatted file

--- src/test/java/edu/ren/geeks/string/UncommonCharsUsingSortTest.java
+++ src/test/java/edu/ren/geeks/string/UncommonCharsUsingSortTest.java
@@ -13,7 +13,7 @@ public class UncommonCharsUsingSortTest {
     public void findUncommonChars_1() {
         Set<Character> actual = uncommonChars.findUncommonChars("hello", "world");
         Character[] expected = {'e', 'h', 'd', 'r', 'w'};
-        assertThat(actual.size(),is(expected.length));
+        assertThat(actual.size(), is(expected.length));
         assertThat(actual, hasItems(expected));
     }
 
@@ -21,7 +21,7 @@ public class UncommonCharsUsingSortTest {
     public void findUncommonChars_2() throws Exception {
         Set<Character> actual = uncommonChars.findUncommonChars("apple", "staple");
         Character[] expected = {'s', 't'};
-        assertThat(actual.size(),is(expected.length));
+        assertThat(actual.size(), is(expected.length));
         assertThat(actual, hasItems(expected));
     }
 
@@ -29,7 +29,7 @@ public class UncommonCharsUsingSortTest {
     public void findUncommonChars_3() {
         Set<Character> actual = uncommonChars.findUncommonChars("characters", "alphabets");
         Character[] expected = {'b', 'c', 'l', 'p', 'r'};
-        assertThat(actual.size(),is(expected.length));
+        assertThat(actual.size(), is(expected.length));
         assertThat(actual, hasItems(expected));
     }
 
@@ -37,7 +37,7 @@ public class UncommonCharsUsingSortTest {
     public void findUncommonChars_4() {
         Set<Character> actual = uncommonChars.findUncommonChars("geeksforgeeks", "geeksquiz");
         Character[] expected = {'f', 'i', 'o', 'q', 'r', 'u', 'z'};
-        assertThat(actual.size(),is(expected.length));
+        assertThat(actual.size(), is(expected.length));
         assertThat(actual, hasItems(expected));
     }
 
@@ -45,14 +45,15 @@ public class UncommonCharsUsingSortTest {
     public void findUncommonChars_5() {
         Set<Character> actual = uncommonChars.findUncommonChars("geeksgeeksfor", "geeksquiz");
         Character[] expected = {'f', 'i', 'o', 'q', 'r', 'u', 'z'};
-        assertThat(actual.size(),is(expected.length));
+        assertThat(actual.size(), is(expected.length));
         assertThat(actual, hasItems(expected));
     }
+
     @Test
     public void findUncommonChars_6() {
         Set<Character> actual = uncommonChars.findUncommonChars("geeks", "geeksforquiz");
         Character[] expected = {'f', 'i', 'o', 'q', 'r', 'u', 'z'};
-        assertThat(actual.size(),is(expected.length));
+        assertThat(actual.size(), is(expected.length));
         assertThat(actual, hasItems(expected));
     }
 
@@ -60,7 +61,23 @@ public class UncommonCharsUsingSortTest {
     public void findUncommonChars_7() {
         Set<Character> actual = uncommonChars.findUncommonChars("andhadhun", "dhun");
         Character[] expected = {'a'};
-        assertThat(actual.size(),is(expected.length));
+        assertThat(actual.size(), is(expected.length));
+        assertThat(actual, hasItems(expected));
+    }
+
+    @Test
+    public void findUncommonChars_8() {
+        Set<Character> actual = uncommonChars.findUncommonChars("abcd", "");
+        Character[] expected = {'a', 'b', 'c', 'd',};
+        assertThat(actual.size(), is(expected.length));
+        assertThat(actual, hasItems(expected));
+    }
+
+    @Test
+    public void findUncommonChars_9() {
+        Set<Character> actual = uncommonChars.findUncommonChars("abcd", "a");
+        Character[] expected = {'b', 'c', 'd',};
+        assertThat(actual.size(), is(expected.length));
         assertThat(actual, hasItems(expected));
     }
 
