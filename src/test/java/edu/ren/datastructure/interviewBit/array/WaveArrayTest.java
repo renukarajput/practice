64562cb27135c6b7f505da2e8984ae6581f83b31
renuka <renukarajput707@gmail.com>
2018-11-20 14:40:14 +0530

    Wave arrayList problem

--- src/test/java/edu/ren/datastructure/interviewBit/array/WaveArrayTest.java
+++ src/test/java/edu/ren/datastructure/interviewBit/array/WaveArrayTest.java
@@ -12,33 +12,41 @@ public class WaveArrayTest {
 
     @Test
     public void waveSort() {
-        Integer[] arr={1,2,3,4};
-        Integer [] expected={2,1,4,3};
-        ArrayList<Integer> result=new WaveArray().waveSort(new ArrayList(Arrays.asList(arr)));
-        assertThat(result,is(new ArrayList(Arrays.asList(expected))));
+        Integer[] arr = {1, 2, 3, 4};
+        Integer[] expected = {2, 1, 4, 3};
+        ArrayList<Integer> result = new WaveArray().waveSort(new ArrayList(Arrays.asList(arr)));
+        assertThat(result, is(new ArrayList(Arrays.asList(expected))));
     }
 
     @Test
     public void waveSort_1() {
-        Integer[] arr={1,6,8,9,5}; //1 6 5 9 8
-        Integer [] expected={5,1,8,6,9};
-        ArrayList<Integer> result=new WaveArray().waveSort(new ArrayList(Arrays.asList(arr)));
-        assertThat(result,is(new ArrayList(Arrays.asList(expected))));
+        Integer[] arr = {1, 6, 8, 9, 5}; //1 6 5 9 8
+        Integer[] expected = {5, 1, 8, 6, 9};
+        ArrayList<Integer> result = new WaveArray().waveSort(new ArrayList(Arrays.asList(arr)));
+        assertThat(result, is(new ArrayList(Arrays.asList(expected))));
     }
 
     @Test
+    public void waveSort_2() {
+        Integer[] arr = {1, 60, 8, 9, 5}; //1 6 5 9 8
+        Integer[] expected = {5, 1, 9, 8, 60};
+        ArrayList<Integer> result = new WaveArray().waveSort(new ArrayList(Arrays.asList(arr)));
+        assertThat(result, is(new ArrayList(Arrays.asList(expected))));
+    }
+
+/*    @Test
     public void waveSort_1_smallest() {
-        Integer[] arr={1,6,8,9,5}; //1 6 5 9 8
-        Integer [] expected={5, 1, 8, 6, 9};
-        ArrayList<Integer> result=new WaveArray().waveSortLexicoGraphicallySmallest(new ArrayList(Arrays.asList(arr)));
-        assertThat(result,is(new ArrayList(Arrays.asList(expected))));
+        Integer[] arr = {1, 6, 8, 9, 5}; //1 6 5 9 8
+        Integer[] expected = {5, 1, 8, 6, 9};
+        ArrayList<Integer> result = new WaveArray().waveSortLexicoGraphicallySmallest(new ArrayList(Arrays.asList(arr)));
+        assertThat(result, is(new ArrayList(Arrays.asList(expected))));
     }
 
     @Test
     public void waveSort_2_smallest() {
-        Integer[] arr={1,60,8,9,5}; //1 5,8,9,60    1 8 5 60 9
-        Integer [] expected={5, 1, 9, 8, 60};
-        ArrayList<Integer> result=new WaveArray().waveSortLexicoGraphicallySmallest(new ArrayList(Arrays.asList(arr)));
-        assertThat(result.toString(),is(new ArrayList(Arrays.asList(expected)).toString()));
-    }
+        Integer[] arr = {1, 60, 8, 9, 5}; //1 5,8,9,60    1 8 5 60 9
+        Integer[] expected = {5, 1, 9, 8, 60};
+        ArrayList<Integer> result = new WaveArray().waveSortLexicoGraphicallySmallest(new ArrayList(Arrays.asList(arr)));
+        assertThat(result.toString(), is(new ArrayList(Arrays.asList(expected)).toString()));
+    }*/
 }
