20f154c39f11f0c2136fbe025e849fffdfcc2c0a
rakesh <rakesh99kr@gmail.com>
2018-11-21 21:57:52 +0530

    min sum path in matrix

---
+++ src/test/java/edu/ren/datastructure/interviewBit/dynamicprogramming/MinSumPathInMatrixTest.java
@@ -0,0 +1,31 @@
+package edu.ren.datastructure.interviewBit.dynamicprogramming;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class MinSumPathInMatrixTest {
+    MinSumPathInMatrix minSumPathInMatrix = new MinSumPathInMatrix();
+
+    @Test
+    public void testMinSum() {
+        Integer[][] grid =
+                        {{1, 3, 2},
+                        {4, 3, 1},
+                        {5, 6, 1}};
+       assertThat(minSumPathInMatrix.minSumPath(grid),is(8));
+    }
+
+
+    @Test
+    public void testMinSum_1() {
+        Integer[][] grid =
+                                {{1, 3, 2},
+                                {4, 3, 1},
+                                {5, 6, 1},
+                                {1,1,1}};
+
+       assertThat(minSumPathInMatrix.minSumPath(grid),is(9));
+    }
+}
