556c2b27b338780cd09d5f2db8e4c869711fc346
renuka <renukarajput707@gmail.com>
2018-11-27 17:01:08 +0530

    polymorphism/inheritance demo

---
+++ src/main/java/edu/ren/inheritance/InheritanceDemo.java
@@ -0,0 +1,43 @@
+package edu.ren.inheritance;
+
+public class InheritanceDemo {
+
+    public static void main(String[] args) {
+        Vehicle bike = new Bike(); // run-time polymorphism
+        Vehicle car = new Car(); // ^ acquired through inheritance ..(method overriding)
+        bike.Start();
+
+        Vehicle v1 = new Vehicle();
+        v1.display("Renuka"); // static polymorphism (method overloading) acquired within same class
+    }
+}
+
+class Vehicle {
+    public void Start() {
+        System.out.println("Start vehicle");
+    }
+
+    // overloading
+    public void display(String str) {
+        System.out.println("str= " + str);
+    }
+
+    public void display(int i) {
+        System.out.println("i = " + i);
+    }
+
+}
+// overriding
+class Bike extends Vehicle{
+    @Override
+    public void Start(){
+        System.out.println("Start Bike");
+    }
+}
+
+class Car extends  Vehicle{
+    @Override
+    public void Start(){
+        System.out.println("Start Car");
+    }
+}
