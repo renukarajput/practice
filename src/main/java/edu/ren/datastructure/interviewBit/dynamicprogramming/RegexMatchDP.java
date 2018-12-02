1224b23f2feaca69ff697a1bed26ba7a108c4834
rakesh <rakesh99kr@gmail.com>
2018-12-02 15:01:09 +0530

    regex match

--- src/main/java/edu/ren/datastructure/interviewBit/dynamicprogramming/RegexMatchDP.java
+++ src/main/java/edu/ren/datastructure/interviewBit/dynamicprogramming/RegexMatchDP.java
@@ -5,14 +5,10 @@ public class RegexMatchDP extends RegexMatch2 {
     boolean isMatch(char[] word, char[] pattern) {
         boolean[][] result = new boolean[word.length + 1][pattern.length + 1];
 
-        if (pattern[0] == word[0] || (pattern[0] == '?') || pattern[0] == '*') {
+         result[0][0] = true;
 
-           // result[0][0] = true;
-            result[0][1] = true;
-        }
-
-        for (int i = 0; i <pattern.length ; i++) {
-            if ('*'== pattern[i]) {
+        for (int i = 1; i <=pattern.length ; i++) {
+            if ('*'== pattern[i-1]) {
                 result[0][i] = true;
             } else {
                 break;
@@ -26,7 +22,7 @@ public class RegexMatchDP extends RegexMatch2 {
             for (int j = 1; j <= pattern.length; j++) {
                 if (pattern[j - 1] == '*') {
                     result[i][j] = result[i][j - 1] || result[i - 1][j];
-                } else if (word[i - 1] == pattern[j - 1] || pattern[j - 1] == '?') {
+                } else if (word[i - 1] == pattern[j - 1] || (pattern[j - 1] == '?')) {
                     result[i][j] = result[i - 1][j - 1];
                 }
             }
