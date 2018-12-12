c2df34037631b28450cd1d9f36e76805268de382
renuka <renukarajput707@gmail.com>
2018-12-12 14:28:41 +0530

    pair of max product, naive method

--- src/main/java/edu/ren/datastructure/interviewBit/array/MaximumProduct.java
+++ src/main/java/edu/ren/datastructure/interviewBit/array/MaximumProduct.java
@@ -19,48 +19,4 @@ public class MaximumProduct {
         }
         return pair;
     }
-
-    public int[] getPairOfMaxProductFromOptimzed(int[] input) {
-        int length = 2;
-        int[] pair = new int[length];
-        int max, smax, min, smin;
-        if (input[0] > input[1]) {
-            max = input[0];
-            smax = input[1];
-        } else {
-            max = input[1];
-            smax = input[0];
-        }
-        min = smax;
-        smin = max;
-        for (int i = 2; i < input.length; i++) {
-            if (max < input[i]) {
-                smax = max;
-                max = input[i];
-            } else {
-                if (smax < input[i]) {
-                    smax = input[i];
-                }
-            }
-
-            if (min > input[i]) {
-                smin = min;
-                min = input[i];
-            } else {
-                if (smin > input[i]) {
-                    smin = input[i];
-                }
-            }
-        }
-        int maxProduct = max * smax;
-        int minProduct = min * smin;
-        if (maxProduct > minProduct){
-            pair[0] = smax;
-            pair[1] = max;
-        } else {
-            pair[0] = smin;
-            pair[1] = min;
-        }
-        return pair;
-    }
 }
