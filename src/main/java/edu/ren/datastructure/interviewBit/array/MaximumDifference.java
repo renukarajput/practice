8e109189bbaf87e5334e2bc444d5e4263af2d07a
renuka <renukarajput707@gmail.com>
2018-12-13 09:56:14 +0530

    Find maximum difference so far(stock buy n sell- maximum profit)

--- src/main/java/edu/ren/datastructure/interviewBit/array/MaximumDifference.java
+++ src/main/java/edu/ren/datastructure/interviewBit/array/MaximumDifference.java
@@ -2,20 +2,38 @@ package edu.ren.datastructure.interviewBit.array;
 
 
 public class MaximumDifference {
+/*
     public int[] getPairOfMaxDiff(int[] input) {
         int length = 2;
         int[] pair = new int[length];
         int maxDiff = 0;
         for (int i = 0; i < input.length; i++) {
-            for (int j = 0; j < input.length; j++) {
+            for (int j = 0; j < i; j++) {
                 int diff = input[i] - input[j];
-                if (maxDiff < diff){
+                if (maxDiff < diff) {
                     maxDiff = diff;
-                    pair[0]=input[i];
-                    pair[1]=input[j];
+                    pair[0] = input[i];
+                    pair[1] = input[j];
                 }
             }
         }
         return pair;
+    }*/
+
+    public int getPairOfMaxDiff(int[] input) {
+        int diff = 0;
+        int maxDiff = input[1] - input[0];
+        int currMin = input[0];
+
+        for (int i = 1; i < input.length; i++) {
+            diff = input[i] - currMin;
+            if (diff > maxDiff) {
+                maxDiff = diff;
+            }
+            if (currMin > input[i]) {
+                currMin = input[i];
+            }
+        }
+        return maxDiff;
     }
 }
--- src/test/java/edu/ren/datastructure/interviewBit/array/MaximumDifferenceTest.java
+++ src/test/java/edu/ren/datastructure/interviewBit/array/MaximumDifferenceTest.java
@@ -1,10 +1,8 @@
 package edu.ren.datastructure.interviewBit.array;
 
 import org.junit.Test;
-
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
-
 
 public class MaximumDifferenceTest {
     MaximumDifference maximumDiff = new MaximumDifference();
@@ -12,22 +10,36 @@ public class MaximumDifferenceTest {
     @Test
     public void input_1() {
         int[] arr = {1, 2, 4, 6, 3, 7, 8};
-        int[] output = maximumDiff.getPairOfMaxDiff(arr);
-        assertThat(output, is(new int[]{8, 1}));
+        int output = maximumDiff.getPairOfMaxDiff(arr);
+        assertThat(output, is(7));
     }
 
     @Test
     public void input_2() {
         int[] arr = {4, 3, 9, 11, 15, 13};
-        int[] output = maximumDiff.getPairOfMaxDiff(arr);
-        assertThat(output, is(new int[]{15, 3}));
+        int output = maximumDiff.getPairOfMaxDiff(arr);
+        assertThat(output, is(12));
     }
 
     @Test
     public void input_3() {
         int[] arr = {4, 3, 1, 9, 11, 15, 13};
-        int[] output = maximumDiff.getPairOfMaxDiff(arr);
-        assertThat(output, is(new int[]{15, 1}));
+        int output = maximumDiff.getPairOfMaxDiff(arr);
+        assertThat(output, is(14));
+    }
+
+    @Test
+    public void input_4() {
+        int[] arr = {9, 4, 7, 3, 6, 1, 2};
+        int output = maximumDiff.getPairOfMaxDiff(arr);
+        assertThat(output, is(3));
+    }
+
+    @Test
+    public void input_5() {
+        int[] arr = {-9, -4, 7, 3, -6, 1, -2};
+        int output = maximumDiff.getPairOfMaxDiff(arr);
+        assertThat(output, is(16)); // 7-(-9)=16
     }
 
 }
