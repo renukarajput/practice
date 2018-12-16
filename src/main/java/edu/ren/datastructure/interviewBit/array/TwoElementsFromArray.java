c6c91f69c30609a7ee6b2b467ade4985c3419414
renuka <renukarajput707@gmail.com>
2018-12-17 00:38:00 +0530

    removed unnecessary code

--- src/main/java/edu/ren/datastructure/interviewBit/array/TwoElementsFromArray.java
+++ src/main/java/edu/ren/datastructure/interviewBit/array/TwoElementsFromArray.java
@@ -35,44 +35,4 @@ public class TwoElementsFromArray {
         }
         return new int[]{};
     }
-
-// 1,2,3,4,5,6,7,8 = 10
-    public int[] findTwoElementsWithSumUsingBinarySearch(int[] input, int sum) {
-        int len = input.length - 1, diff;
-        for (int i = 0; i <= len; i++) {
-            diff = sum - input[i];
-            if (input[i] == diff) {
-                return new int[]{input[i], diff};
-            }
-        }
-        return new int[]{};
-    }
-}
-
-
-class Pair {
-    int diff, Elem;
-
-    public Pair(int diff, int elem) {
-        this.diff = diff;
-        Elem = elem;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Pair pair = (Pair) o;
-
-        if (diff != pair.diff) return false;
-        return Elem == pair.Elem;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = diff;
-        result = 31 * result + Elem;
-        return result;
-    }
 }
